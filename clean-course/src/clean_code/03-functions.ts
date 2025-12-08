81327e839119eee2a8fe26decdc1b0910a67c4bc
caplascencia <ca.plascencia@mavi.mx>
2025-12-08 09:20:42 -0600

    SECCIÓN 2 / CAP 9 - EN CURSO

---
+++ clean-course/src/clean_code/03-functions.ts
@@ -0,0 +1,40 @@
+(() => {
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioById(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
+  }
+
+  // Crear una película
+  function createMovie({ title, cast, description, rating }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  async function createActor(
+    fullName: string,
+    birthdate: Date
+  ): Promise<boolean> {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "fernando") return false;
+
+    console.log("Crear actor");
+    return true;
+  }
+})();
